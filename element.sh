5a315fb69a29c9d60d4580e0739938b30477ff5c
Ben Collen <ben.collen@me.com>
2024-10-12 16:38:48 +0000

    feat: made script more readable

--- element.sh
+++ element.sh
@@ -11,7 +11,7 @@ input="$1"
 
 # Check if the input is a number (atomic number) or a string (symbol or name)
 if [[ $input =~ ^[0-9]+$ ]]; then
-  # Input is a number, search by atomic number
+  # Input is a number, search by atomic number.
   element=$(psql --username=freecodecamp --dbname=periodic_table -t --no-align -c "
     SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius
     FROM elements
@@ -20,7 +20,7 @@ if [[ $input =~ ^[0-9]+$ ]]; then
     WHERE atomic_number = $input;
   ")
 else
-  # Input is a string, search by symbol or name
+  # Input is a string, search by symbol or name.
   element=$(psql --username=freecodecamp --dbname=periodic_table -t --no-align -c "
     SELECT atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius
     FROM elements
@@ -30,13 +30,3 @@ else
   ")
 fi
 
-# Check if element was found
-if [[ -z $element ]]; then
-  echo "I could not find that element in the database."
-else
-  # Parse the element information
-  IFS="|" read atomic_number name symbol type mass melting_point boiling_point <<< "$element"
-
-  # Output the required information
-  echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
-fi
